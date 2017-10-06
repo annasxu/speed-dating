c8965250f9b6d68f8124bbb19b1f24f39b38f6f6
mdb293 <mdb293@cornell.edu>
2017-10-06 17:36:31 -0400

    added age diff

--- Speed-Dating.R
+++ Speed-Dating.R
@@ -1,6 +1,7 @@
 Speed_Dating_Data = read.csv("Speed Dating Data.csv")
 
 data =Speed_Dating_Data
+age_diff = data$age - data$age_o
 data = data.frame(data, age_diff)
 
 #setting aside 7 waves for test set
@@ -11,8 +12,4 @@ Dtest = subset(data,wave %in% test)
 #14 waves in training set
 Dtrain = subset(data, !(wave %in% test))
 
-model = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb+Speed_Dating_Data$shar,data=data)
-summary(model)
-
-model2 = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb,data=data)
-summary(model2)
+plot(Dtrain$age_diff,Dtrain$)
