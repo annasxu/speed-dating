d6fb2f2561b64f02884ef780065844619d7fb64d
mdb293 <mdb293@cornell.edu>
2017-10-06 17:30:40 -0400

    Split data into test and train

--- Speed-Dating.R
+++ Speed-Dating.R
@@ -1,18 +1,18 @@
-<<<<<<< HEAD
 Speed_Dating_Data = read.csv("Speed Dating Data.csv")
-=======
-Speed_Dating_Data <- read.csv("~/Desktop/speed-dating-experiment/Speed Dating Data.csv")
->>>>>>> 76b7e572c99ccf02c2cffc71f3ac5d09e7f7176d
-data =data.frame(Speed_Dating_Data$like,Speed_Dating_Data$samerace,Speed_Dating_Data$int_corr)
 
-data = na.omit(data)
-data
+data =Speed_Dating_Data
+data = data.frame(data, age_diff)
+
+#setting aside 7 waves for test set
+set.seed(6)
+test = sample(21,7)
+Dtest = subset(data,wave %in% test)
+
+#14 waves in training set
+Dtrain = subset(data, !(wave %in% test))
 
 model = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb+Speed_Dating_Data$shar,data=data)
 summary(model)
-<<<<<<< HEAD
-=======
 
 model2 = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb,data=data)
 summary(model2)
->>>>>>> 76b7e572c99ccf02c2cffc71f3ac5d09e7f7176d
