76b7e572c99ccf02c2cffc71f3ac5d09e7f7176d
Anna Xu <annasxu@Annas-MacBook-Pro.local>
2017-10-06 16:23:40 -0400

    secondmodel

--- Speed-Dating.R
+++ Speed-Dating.R
@@ -1,4 +1,4 @@
-Speed_Dating_Data <- read_csv("~/Desktop/speed-dating-experiment/Speed Dating Data.csv")
+Speed_Dating_Data <- read.csv("~/Desktop/speed-dating-experiment/Speed Dating Data.csv")
 data =data.frame(Speed_Dating_Data$like,Speed_Dating_Data$samerace,Speed_Dating_Data$int_corr)
 
 data = na.omit(data)
@@ -6,3 +6,6 @@ data
 
 model = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb+Speed_Dating_Data$shar,data=data)
 summary(model)
+
+model2 = lm(Speed_Dating_Data$like~Speed_Dating_Data$samerace+Speed_Dating_Data$int_corr+Speed_Dating_Data$attr+Speed_Dating_Data$sinc+Speed_Dating_Data$intel+Speed_Dating_Data$fun+Speed_Dating_Data$amb,data=data)
+summary(model2)
